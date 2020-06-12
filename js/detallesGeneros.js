bb968bc280807873cb0360101625acc9238341cd
BelenVincent <beluvincent@hotmail.com>
2020-06-12 20:26:40 -0300

    artistas del genero

--- js/detallesGeneros.js
+++ js/detallesGeneros.js
@@ -37,19 +37,26 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/' + Gene
 
         let contenedorartistas = document.querySelector('.contenedor-artistas');
        let artistas = datos.data
+
+       let contador = 0
+
        for (const artista of artistas) {
+
            contenedorartistas.innerHTML+=`
            <div>
         <div class="artista-name">
             
            <a href="detallesArtista.html?id=${artista.id}">${artista.name} </a>
         </div>
-   </div>
-           
-           `
-       }
-        
+   </div>`
 
+    contador ++;
+    
+   if (contador == 10){
+    break;
+          }  
+       }
+     
     })
     .catch(function(error){
         console.error(error);
